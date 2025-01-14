7b8676a1241c8fa8c9ecd591047e276a4a937d46
Hakkenheiner <remi.zicken@gmail.com>
2025-01-14 10:41:14 +0100

    finish step5

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -13,7 +13,7 @@ class BoatRepository {
   async readAll(where = {}) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      "select b.id, b.name, b.coord_x, b.coord_y, t.type, t.has_treasure from boat b inner join tile t on b.coord_x = t.coord_x and b.coord_y = t.coord_y",
     );
 
     // Return the array of tiles
