b06500ce3d98529730abb4ead9ecd38cdaa36b91
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2025-01-14 11:20:36 +0100

    step5 done

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -11,10 +11,21 @@ type Boat = {
 class BoatRepository {
   async readAll(where = {}) {
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      `SELECT 
+        boat.id,
+        boat.coord_x,
+        boat.coord_y,
+        boat.name,
+        tile.type,
+        tile.has_treasure
+      FROM boat
+      JOIN tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y`,
     );
 
-    return rows as Boat[];
+    return rows as (Boat & {
+      type: string;
+      has_treasure: boolean;
+    })[];
   }
 
   async update(boatToUpdate: Partial<Boat>) {
