442e8b552353dfa4b8fa3a54523d377f3ebb5936
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2025-01-14 10:48:37 +0100

    step2 done

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,14 +1,40 @@
 import type { RequestHandler } from "express";
+import database from "../../../database/client";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const [tiles] = await database.query("SELECT * FROM tile");
+    res.json(tiles);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
   // your code here
 };
 
+const read: RequestHandler = async (req, res, next) => {
+  // your code here
+};
+
+const edit: RequestHandler = async (req, res, next) => {
+  // your code here
+};
+
+const add: RequestHandler = async (req, res, next) => {
+  // your code here
+};
+
+const destroy: RequestHandler = async (req, res, next) => {
+  // your code here
+};
+
 export default {
   browse,
+  read,
+  edit,
+  add,
   validate,
+  destroy,
 };
