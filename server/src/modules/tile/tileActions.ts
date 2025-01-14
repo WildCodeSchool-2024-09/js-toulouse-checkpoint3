d80c408ae1b3d8a932930f70d26c0c1f5794c6c0
Jujock004 <julien.joecker@gmail.com>
2025-01-14 10:22:39 +0100

    step2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,14 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    const tiles = await tileRepository.readAll();
+    res.status(200).json(tiles);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
